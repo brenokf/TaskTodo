79dd82f689cbb022a91d7fa3fbe67f9341637d1b
Breno Melo Mendes <breno.mm@kyndryl.com>
2024-02-12 12:21:22 -0400

    create new test Input

--- cypress/e2e/firstTest.cy.ts
+++ cypress/e2e/firstTest.cy.ts
@@ -1,11 +1,15 @@
 describe('template spec', () => {
-  it('passes', () => {
+  it('Do visit site', () => {
     cy.visit('http://localhost:5173/')
   })
-})
-
-describe('Do have test a input ', () => {
-  it('Imput Empty', () => {
-      cy.get('form').should('have.value').contains('empty')
-  }),
+  // it('Do submit with input Empty', () => {
+  //   cy.visit('http://localhost:5173/')
+  //   cy.findByRole('textbox').clear();
+  //   cy.findByRole('button').click();
+  // })
+  it('Do Create New Task', () => {
+    cy.visit('http://localhost:5173/')
+    cy.findByRole('textbox').type('Criar um esboço de um novo logotipo para uma empresa fictícia de tecnologia.');
+    cy.findByRole('button').click();
+  })
 })
